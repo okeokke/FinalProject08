552208ac1b9cd1d1eefda04a8a6695f7010c96b0
NYANTUY <nyantuyskuy12@gmail.com>
2025-09-28 22:24:42 +0700

    Commit Message

---
+++ Test01.java
@@ -0,0 +1,7 @@
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("siap");
+        System.out.println("Hello, ini baris kode pertama saya");
+    }
+    
+}
